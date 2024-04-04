8db988908257f1e5f8e2f021be940eb108854e12
Maja Kabus <mkabus@cern.ch>
2024-04-04 21:34:45 +0200

    Add collision-only mixing benchmarks

--- Framework/Core/test/benchmark_EventMixing.cxx
+++ Framework/Core/test/benchmark_EventMixing.cxx
@@ -26,8 +26,10 @@ using namespace o2::soa;
 
 #ifdef __APPLE__
 constexpr unsigned int maxPairsRange = 8;
+constexpr unsigned int maxColPairsRange = 13;
 #else
 constexpr unsigned int maxPairsRange = 8;
+constexpr unsigned int maxColPairsRange = 15;
 #endif
 constexpr int numEventsToMix = 5;
 constexpr int numTracksPerEvent = 10000;
@@ -196,5 +198,114 @@ static void BM_EventMixingCombinations(benchmark::State& state)
 }
 
 BENCHMARK(BM_EventMixingCombinations)->RangeMultiplier(2)->Range(4, 2 << maxPairsRange);
+
+static void BM_EventMixingNaiveCollisionsPairsSameCategories(benchmark::State& state)
+{
+  // Seed with a real random value, if available
+  std::default_random_engine e1(1234567891);
+  std::uniform_real_distribution<float> uniform_dist(0.f, 1.f);
+  std::uniform_real_distribution<float> uniform_dist_x(-0.065f, 0.073f);
+  std::uniform_real_distribution<float> uniform_dist_y(-0.320f, 0.360f);
+  std::uniform_int_distribution<int> uniform_dist_int(0, 5);
+
+  std::vector<double> xBins{VARIABLE_WIDTH, -0.064, -0.062, -0.060, 0.066, 0.068, 0.070, 0.072};
+  std::vector<double> yBins{VARIABLE_WIDTH, -0.320, -0.301, -0.300, 0.330, 0.340, 0.350, 0.360};
+  using BinningType = ColumnBinningPolicy<o2::aod::collision::PosX, o2::aod::collision::PosY>;
+  BinningType binningOnPositions{{xBins, yBins}, true}; // true is for 'ignore overflows' (true by default)
+
+  TableBuilder colBuilder;
+  auto rowWriterCol = colBuilder.cursor<o2::aod::Collisions>();
+  for (auto i = 0; i < state.range(0); ++i) {
+    float x = uniform_dist_x(e1);
+    float y = uniform_dist_y(e1);
+    rowWriterCol(0, uniform_dist_int(e1),
+                 x, y, uniform_dist(e1),
+                 uniform_dist(e1), uniform_dist(e1), uniform_dist(e1),
+                 uniform_dist(e1), uniform_dist(e1), uniform_dist(e1),
+                 uniform_dist_int(e1), uniform_dist(e1),
+                 uniform_dist_int(e1),
+                 uniform_dist(e1), uniform_dist(e1));
+  }
+  auto tableCol = colBuilder.finalize();
+  o2::aod::Collisions collisions{tableCol};
+
+  int64_t colCount = 0;
+  int nBinsTot = (xBins.size() - 2) * (yBins.size() - 2);
+
+  for (auto _ : state) {
+    colCount = 0;
+    int n = state.range(0);
+    std::vector<std::list<o2::aod::Collisions::iterator>> mixingBufferVector;
+    for (int i = 0; i < nBinsTot; i++) {
+      mixingBufferVector.push_back(std::list<o2::aod::Collisions::iterator>());
+    }
+    for (auto& col1 : collisions) {
+      int bin = binningOnPositions.getBin({col1.posX(), col1.posY()});
+      if (bin == -1) {
+        continue;
+      }
+      auto& mixingBuffer = mixingBufferVector[bin];
+      if (mixingBuffer.size() > 0) {
+        for (auto& col2 : mixingBuffer) {
+          colCount++;
+        }
+        if (mixingBuffer.size() >= numEventsToMix) {
+          mixingBuffer.pop_back();
+        }
+      }
+      mixingBuffer.push_front(col1);
+    }
+    benchmark::DoNotOptimize(colCount);
+  }
+  state.counters["Mixed collision pairs"] = colCount;
+  state.SetBytesProcessed(state.iterations() * sizeof(float) * colCount);
+}
+
+BENCHMARK(BM_EventMixingNaiveCollisionsPairsSameCategories)->RangeMultiplier(2)->Range(4, 2 << maxColPairsRange);
+
+static void BM_EventMixingCombGenCollisionsPairsSameCategories(benchmark::State& state)
+{
+  // Seed with a real random value, if available
+  std::default_random_engine e1(1234567891);
+  std::uniform_real_distribution<float> uniform_dist(0.f, 1.f);
+  std::uniform_real_distribution<float> uniform_dist_x(-0.065f, 0.073f);
+  std::uniform_real_distribution<float> uniform_dist_y(-0.320f, 0.360f);
+  std::uniform_int_distribution<int> uniform_dist_int(0, 5);
+
+  std::vector<double> xBins{VARIABLE_WIDTH, -0.064, -0.062, -0.060, 0.066, 0.068, 0.070, 0.072};
+  std::vector<double> yBins{VARIABLE_WIDTH, -0.320, -0.301, -0.300, 0.330, 0.340, 0.350, 0.360};
+  using BinningType = ColumnBinningPolicy<o2::aod::collision::PosX, o2::aod::collision::PosY>;
+  BinningType binningOnPositions{{xBins, yBins}, true}; // true is for 'ignore overflows' (true by default)
+
+  TableBuilder colBuilder;
+  auto rowWriterCol = colBuilder.cursor<o2::aod::Collisions>();
+  for (auto i = 0; i < state.range(0); ++i) {
+    float x = uniform_dist_x(e1);
+    float y = uniform_dist_y(e1);
+    rowWriterCol(0, uniform_dist_int(e1),
+                 x, y, uniform_dist(e1),
+                 uniform_dist(e1), uniform_dist(e1), uniform_dist(e1),
+                 uniform_dist(e1), uniform_dist(e1), uniform_dist(e1),
+                 uniform_dist_int(e1), uniform_dist(e1),
+                 uniform_dist_int(e1),
+                 uniform_dist(e1), uniform_dist(e1));
+  }
+  auto tableCol = colBuilder.finalize();
+  o2::aod::Collisions collisions{tableCol};
+
+  int64_t colCount = 0;
+
+  for (auto _ : state) {
+    colCount = 0;
+    for (auto& comb : combinations(CombinationsBlockUpperSameIndexPolicy(binningOnPositions, numEventsToMix - 1, -1, collisions, collisions))) {
+      colCount++;
+    }
+    benchmark::DoNotOptimize(colCount);
+  }
+  state.counters["Mixed collision pairs"] = colCount;
+  state.SetBytesProcessed(state.iterations() * sizeof(float) * colCount);
+}
+
+BENCHMARK(BM_EventMixingCombGenCollisionsPairsSameCategories)->RangeMultiplier(2)->Range(4, 2 << maxColPairsRange);
 
 BENCHMARK_MAIN();
